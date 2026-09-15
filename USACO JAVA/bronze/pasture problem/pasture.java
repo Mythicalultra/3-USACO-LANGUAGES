a3728c42e3aa6ba61aeaf5f295edac742a20b3fa
Jiang <keljiang@iu.edu>
2026-09-15 19:30:25 -0400

    Actually solved now

--- USACO JAVA/bronze/pasture problem/pasture.java
+++ USACO JAVA/bronze/pasture problem/pasture.java
@@ -1,11 +1,11 @@
-//import Java.io.*;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class pasture {
     public static void main(String[] args) throws Exception {
-        //System.setIn(new FileInputStream("Square.in"));
-        //System.setOut(new PrintStream(new FileOutputStream("Square.out")));
+        System.setIn(new FileInputStream("Square.in"));
+        System.setOut(new PrintStream(new FileOutputStream("Square.out")));
 
         Scanner input = new Scanner(System.in);
 
@@ -17,10 +17,14 @@ public class pasture {
 
     List<Integer> rect1 = Arrays.stream(string1.split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
     List<Integer> rect2 = Arrays.stream(string2.split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
-
-    System.out.println(rect1);
-    System.out.println(rect2);
     
     int lowx = Math.min(rect1.get(0), rect2.get(0));
+    int highx = Math.max(rect1.get(2), rect2.get(2));
+
+    int lowy = Math.min(rect1.get(1), rect2.get(1));
+    int highy = Math.max(rect1.get(3), rect2.get(3));
+
+    int area = Math.max(highx - lowx, highy - lowy) * Math.max(highx - lowx, highy - lowy)
+    System.out.println(area);
     }
 }
